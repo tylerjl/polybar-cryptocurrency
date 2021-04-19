746ecdacc171eaf6246f02737b5fa3f43e6a4f3c
Tyler Langlois <tjl@byu.net>
2021-04-18 21:14:54 -0600

    Polybar color improvements

--- pcrypto.py
+++ pcrypto.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import requests
 import argparse
+import requests
 import json
 import sys
+import locale
 import os
 
 parser = argparse.ArgumentParser(description="Display currencies on polybar")
@@ -15,6 +16,8 @@ parser.add_argument("--decimals", type=int,
                     nargs="?", default=2, help="How many decimals to show")
 parser.add_argument("--display", type=str,
                     nargs="?", default="price", choices=["price", "percentage", "both"], help="Display mode")
+parser.add_argument("--polycolor", action='store_true',
+                    help="Whether to display polybor color codes")
 
 args = parser.parse_args()
 home = os.path.expanduser("~/")
@@ -25,12 +28,22 @@ with open(f"{home}.config/polybar/coins.svg", "r", encoding="utf-8") as icons:
         unicode, coin = line.strip().split(":")
         unicode_dict[unicode] = coin
 
+def colorize(n):
+    if args.polycolor:
+        if n >= 0:
+            return "%{F#32cd32}"+ f"{n:+}%" + "%{F-}"
+        else:
+            return "%{F#ff6347}" + f"{n:+}%" + "%{F-}"
+    else:
+        return f"{n:+}%"
+
+locale.setlocale(locale.LC_ALL, locale='en_US.utf8')
 
 for coin in args.coins:
     get = requests.get(
         f"https://api.coinranking.com/v1/public/coins?prefix={coin}&base={args.base}").json()["data"]
     price_float = round(float(get["coins"][0]["price"]), args.decimals)
-    current_price = get["base"]["sign"] + str(price_float)
+    current_price = locale.currency(price_float, grouping=True)
     change = get["coins"][0]["change"]
 
     for _unicode, _coin in unicode_dict.items():
@@ -41,4 +54,4 @@ for coin in args.coins:
             if args.display == "percentage":
                 sys.stdout.write(f"  {icon} {change:+}% ")
             if args.display == "both":
-                sys.stdout.write(f" {icon} {current_price} | {change:+}% ")
+                sys.stdout.write(f" {icon} {current_price}/{colorize(change)} ")
